23d4dec55cebb95f6a245a22d016135a88753fa7
Abhyudaya Tomer <67411747+abhyudayat@users.noreply.github.com>
2025-11-26 12:56:24 +0000

    llm parser better model

--- llm_parser.py
+++ llm_parser.py
@@ -9,7 +9,7 @@ class LLMParser:
     with a supported chat model.
     """
 
-    def __init__(self, model="meta-llama/Llama-3.2-1B-Instruct"):
+    def __init__(self, model="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"):
         self.model = model
         self.api_key = os.getenv("HF_API_KEY")
         if not self.api_key:
@@ -24,12 +24,21 @@ class LLMParser:
         """
 
         system_prompt = """
-You are a trading command parser for a Binance Futures bot.
-
-Convert the user's message into a STRICT JSON object with keys:
-
+You are a Trader who knows the trading keywords for a Binance Futures bot.
+You have to parse the user input into a JSON.
+Convert the user's message into a STRICT JSON object by:
+1)  Extracting order type which could be only among th following:
+    market, limit, stop_limit, oco or twap
+2)  Extracting the coin symbol like BTCUSDT for bitcoin, etc
+3)  Extracting the side i.e. SELL or BUY
+4)  Extracting quantity like 0.1, 0.3, 3, 10, etc.
+5)  Extracting price which would be an integer or real number like 3200, 1000.15, etc.
+6)  EXtracting the stop price which would a integer linked to Stop price and mentioned after it in the user input.
+    *(It cannot not be same a price)
+7)  Extracting the no. of intervals of time unit mentioned for twap order duration.
+8)  Extracting the time unit mentioned for the twap order (like )
 REQUIRED:
-- order_type         (market, limit, stop_limit, oco, twap)
+- order_type         (Strictly: market, limit, stop_limit, oco or twap)
 - symbol             (convert to UPPERCASE Binance format, e.g. btc→BTCUSDT)
 - side               (BUY or SELL)
 - quantity
@@ -45,6 +54,27 @@ RULES:
 - If user writes only base asset (e.g. btc), assume USDT pair.
 - Only return valid JSON.
 - No explanations, no text outside JSON.
+Example:
+$ python bot.py "sell 1 eth limit at 3200"
+-order_type = limit       
+- symbol    = ETHUSDT         BTCUSDT)
+- side      = SELL
+- quantity  = 1
+- price     = 3200
+
+$ python bot.py "twap buy btc amount 0.3"
+- order_type = twap      
+- symbol    = BTCUSDT        BTCUSDT)
+- side      = BUY
+- quantity  = 0.3
+
+$ python bot.py "sell 0.5 eth oco for 2700 stop_price 3200"
+- order type = oco
+- symbol     = ETHUSDT
+- side       = SELL
+- Quantity   = 0.5
+- Price      = 2700
+- stop_price = 3200
 """
 
 
@@ -56,8 +86,8 @@ RULES:
                 {"role": "system", "content": system_prompt},
                 {"role": "user", "content": user_prompt}
             ],
-            "temperature": 0,
-            "max_tokens": 200,
+            "temperature": 0.0,
+            "max_tokens": 1000,
         }
 
         try:
